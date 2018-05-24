66442fe1ed18ba9280762a226d02818a1ce34a72
jmeuriss <jean-roch.meurisse@unamur.be>
2018-05-24 14:45:41 +0200

    add maturity information

--- version.php
+++ version.php
@@ -26,6 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018052400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->maturity = MATURITY_BETA;      // Will be changed to STABLE on acceptance.
 $plugin->requires  = 2017110800;        // Requires this Moodle version.
 $plugin->component = 'format_collapsibletopics';    // Full name of the plugin (used for diagnostics).
 $plugin->release = '1.0';
