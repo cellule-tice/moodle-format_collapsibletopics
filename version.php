5dfd28f0f59365c324ea673eccc6562865d05f00
Jean-Roch Meurisse <jean-roch.meurisse@unamur.be>
2019-05-22 11:34:29 +0200

    update version number to last moodle release - commpatibility checked

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018120200;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019052000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires  = 2017110800;        // Requires this Moodle version.
 $plugin->component = 'format_collapsibletopics';    // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.6'; // Align version number with most recent moodle compatible version.
+$plugin->release = '3.7'; // Align version number with most recent moodle compatible version.
